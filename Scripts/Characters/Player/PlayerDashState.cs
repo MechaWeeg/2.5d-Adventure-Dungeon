0babe0532437471983ce503542a91b97134cbde9
MechaWeeg <mechaweeg@gmail.com>
2024-08-14 13:12:59 +0200

    Initial Commit
    Player character & State Machine

---
+++ Scripts/Characters/Player/PlayerDashState.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class PlayerDashState : PlayerState
+{
+    [Export(PropertyHint.Range, "0, 20, 0.1")] private float speed = 10;
+    [Export] private Timer dashTimerNode;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        dashTimerNode.Timeout += HandleDashTimeout;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        characterNode.MoveAndSlide();
+        characterNode.Flip();
+    }
+
+    protected override void EnterState()
+    {
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_DASH);
+        characterNode.Velocity = new(
+            characterNode.direction.X, 0, characterNode.direction.Y
+        );
+
+        if (characterNode.Velocity == Vector3.Zero) 
+        {
+            characterNode.Velocity = characterNode.Sprite3DNode.FlipH ?
+                Vector3.Left :
+                Vector3.Right;
+        }
+
+        characterNode.Velocity *= speed;
+        dashTimerNode.Start();
+    }
+
+    private void HandleDashTimeout()
+    {
+        characterNode.Velocity = Vector3.Zero;
+        characterNode.StateMachineNode.SwitchState<PlayerIdleState>();
+    }
+}
