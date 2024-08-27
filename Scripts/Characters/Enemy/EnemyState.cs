711aa9262d7e2603b43a8654589527681e98e38a
MechaWeeg <mechaweeg@gmail.com>
2024-08-27 17:44:44 +0200

    Combat system

--- Scripts/Characters/Enemy/EnemyState.cs
+++ Scripts/Characters/Enemy/EnemyState.cs
@@ -5,6 +5,14 @@ public abstract partial class EnemyState : CharacterState
 {
     protected Vector3 destination;
 
+    public override void _Ready()
+    {
+        base._Ready();
+
+        characterNode.GetStatResource(Stat.Health).OnZero += HandleZeroHealth;
+    }
+
+
     protected Vector3 GetPointGlobalPosition(int index)
     {
         Vector3 localPosition = characterNode.PathNode.Curve.GetPointPosition(index);
@@ -25,5 +33,9 @@ public abstract partial class EnemyState : CharacterState
     protected void HandleChaseAreaBodyEntered(Node3D body) 
     {
         characterNode.StateMachineNode.SwitchState<EnemyChaseState>();
+    }
+    private void HandleZeroHealth()
+    {
+        characterNode.StateMachineNode.SwitchState<EnemyDeathState>();
     }
 }
