778f3f6348752b7722e3bba60a776e75b8eb0a3c
MechaWeeg <mechaweeg@gmail.com>
2024-09-06 13:53:33 +0200

    Environment and lighting

--- Scripts/Reward/TresureChest.cs
+++ Scripts/Reward/TresureChest.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class tresure_chest : StaticBody3D
+public partial class TresureChest : StaticBody3D
 {
     [Export] private Area3D areaNode;
     [Export] private Sprite3D spriteNode;
